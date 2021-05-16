b9fe47d5c76c0016b8909d13f6f379abb450a7a9
thaontt0201 <athng002@edu.xamk.fi>
2021-05-16 15:04:18 +0300

    part2.9

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "0123456789" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [findName, setFindName] = useState([]);
 
   const addContact = (e) => {
     e.preventDefault();
@@ -27,10 +31,33 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleFindName = (e) => {
+    const correctName = persons.filter(
+      (person) =>
+        person.name.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
+    );
+    console.log(e.target.value);
+    if (correctName.length === 0 || e.target.value === "") {
+      return setFindName([]);
+    }
+    setFindName(correctName);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input onChange={handleFindName} />
+      </div>
+      <div>
+        {findName.map((isName) => (
+          <p key={isName.name}>
+            {isName.name} {isName.number}
+          </p>
+        ))}
+      </div>
       <form onSubmit={addContact}>
+        <h2>Add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
