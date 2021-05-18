2ba915f232b17f765d981c7ce96e29f684627f41
thaontt0201 <athng002@edu.xamk.fi>
2021-05-18 12:35:01 +0300

    part2.11

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Contact from "./components/Contact";
 import Form from "./components/Form";
 import Search from "./components/Search";
+import axios from "axios";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [findName, setFindName] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((respone) => {
+      setPersons(respone.data);
+    });
+  }, []);
 
   const addContact = (e) => {
     e.preventDefault();
