0d2875727e2e02545a4a795ee98eb1139371d42e
thaontt0201 <athng002@edu.xamk.fi>
2021-06-02 13:46:50 +0300

    part3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :response-time ms - :res[content-length] :body")
+);
 
 let persons = [
   {
