1fed9107b0f38e42014fb8adbe6a9e412f17bdbd
thaontt0201 <athng002@edu.xamk.fi>
2021-06-22 15:44:23 +0300

    part4.4

--- part3/blog-list/utils/list_helper.js
+++ part3/blog-list/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((a, b) => {
+    return a + b.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
